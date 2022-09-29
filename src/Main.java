08ede379accb52bebc923becc723e12f0663f0d5
Flacis <flacisphone@mail.ru>
2022-09-29 17:02:44 +0300

    My first app

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Моё имя Евгений");
+        int age = 31;
+        int yearOfBirth = 1991;
+        int value1 = age;
+        int value2 = yearOfBirth;
+        System.out.println("Какой год сейчас, если известен год рождения " + yearOfBirth + " и известно сколько лет " + age);
+        System.out.println(value1 + value2);
+    }
+}
